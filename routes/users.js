e95fb998d755c50b934dfb9d47c54daca2eee8ab
Brainstorm <khameleonstorm@gmail.com>
2023-02-15 00:38:04 +0100

    referral4

--- routes/users.js
+++ routes/users.js
@@ -43,14 +43,13 @@ router.post('/signup', async (req, res) => {
   const { error } = validateUser(req.body)
   if(error) return res.status(400).send(error.details[0].message)
 
-  let user = await User.findOne({
-    $and: [{email}, {username}]
-  })
+  let user = await User.findOne({ $and: [{email}, {username}] })
   if(user) return res.status(400).send("User already exists..")
 
-  user = new User({ username, email, password })
-  
   let referredBy = User.findOne({ referral })
+  if(!referredBy) return res.status(400).send("Invalid referral code..")
+
+  user = new User({ username, email, password })
 
   try{
     const salt = await bcrypt.genSalt(10)
@@ -58,8 +57,8 @@ router.post('/signup', async (req, res) => {
     user = await user.save()
     const token = await user.genAuthToken()
     referredBy.referralBonus ++
-    await referredBy.save()
-    res.header("x-auth-token", token).send(user)
+    referredBy = await referredBy.save()
+    res.header("x-auth-token", token).send({referredBy, user})
   }
   catch(e){
     for(i in e.errors){
